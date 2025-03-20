40aa4fca384eeaebf9719c2dab36cc65ff3051d4
mjb90 <manasborah90@gmail.com>
2025-03-20 15:34:28 +0800

    solidity styling

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,8 +1,46 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+/**
+ * @title Raffle
+ * @author Manas
+ * @dev Implements Chainlink VRFv2.5
+ */
 contract Raffle {
-    constructor() {
-        
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        // Enter the raffle
+    }
+
+    function pickWinner() public {}
+
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
     }
 }
